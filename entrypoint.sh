11a3e09a2c8441cb8df682bcc583a8637a0e3b1b
David Muench <dave@muench.us>
2021-07-16 11:22:17 -0700

    Update ookla repositories to use the new packagecloud location instead of bintray

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 # Install speedtest-cli
 if [ ! -e /usr/bin/speedtest ]
 then
-  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-  echo "deb https://ookla.bintray.com/debian buster main" | tee  /etc/apt/sources.list.d/speedtest.list
+  curl -L "https://packagecloud.io/ookla/speedtest-cli/gpgkey" 2> /dev/null | apt-key add -
+  echo "deb https://packagecloud.io/ookla/speedtest-cli/debian/ buster main" | tee  /etc/apt/sources.list.d/ookla_speedtest-cli.list
   apt-get update && apt-get -q -y install speedtest
   apt-get -q -y autoremove && apt-get -q -y clean
   rm -rf /var/lib/apt/lists/*
