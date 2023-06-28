f53537bf68aa3025210419c82174a2e481a53f37
lukaskroczek <33688158+lukaskroczek@users.noreply.github.com>
2023-06-28 14:32:37 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,7 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 # Install speedtest-cli
 if [ ! -e /usr/bin/speedtest ]
 then
-  curl -L "https://packagecloud.io/ookla/speedtest-cli/gpgkey" 2> /dev/null | apt-key add -
-  #echo "deb https://packagecloud.io/ookla/speedtest-cli/debian/ buster main" | tee  /etc/apt/sources.list.d/ookla_speedtest-cli.list
+  curl -fsSL https://packagecloud.io/ookla/speedtest-cli/gpgkey | gpg --dearmor > /etc/apt/keyrings/ookla_speedtest-cli-archive-keyring.gpg
   curl -s https://install.speedtest.net/app/cli/install.deb.sh 2> /dev/null | bash
   apt-get update && apt-get -q -y install speedtest
   apt-get -q -y autoremove && apt-get -q -y clean
