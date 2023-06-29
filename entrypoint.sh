afb70ace8f197df694f9105cc2d9bec708f1d184
lukaskroczek <33688158+lukaskroczek@users.noreply.github.com>
2023-06-29 11:28:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,10 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 # Install speedtest-cli
 if [ ! -e /usr/bin/speedtest ]
 then
+  mkdir -p /etc/apt/keyrings
+  chmod 755 /etc/apt/keyrings
   curl -fsSL https://packagecloud.io/ookla/speedtest-cli/gpgkey | gpg --dearmor > /etc/apt/keyrings/ookla_speedtest-cli-archive-keyring.gpg
+  
   curl -s https://install.speedtest.net/app/cli/install.deb.sh 2> /dev/null | bash
   apt-get update && apt-get -q -y install speedtest
   apt-get -q -y autoremove && apt-get -q -y clean
